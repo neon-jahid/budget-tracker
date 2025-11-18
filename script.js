2a5feb52a4d7b8628427323e134f6650b32178de
Jahid Hasan <jahid.softwindtech@gmail.com>
2025-11-18 19:57:56 +0600

    worked on localstorage

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+const form = document.querySelector('.transaction-form');
 
+let transactions = [];
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const transaction = {
+        source: form.source.value,
+        amount: form.amount.value,
+    };
+    transactions.push(transaction);
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+    form.reset();
+});
